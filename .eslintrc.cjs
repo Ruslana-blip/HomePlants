c8ac86a9d244368fb5c30d4a0cedec87ea1690e4
Lauta Ruslana <lauta.ruslana@ukr.net>
2024-11-01 20:46:59 +0200

    second commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,9 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
+  publicPath: `/HomePlants/`,
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
